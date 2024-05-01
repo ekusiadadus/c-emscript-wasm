7c290a0954568893e044a1863ff4ea2da327b2f8
ekusiadadus <ekusiadadus@gmail.com>
2024-05-02 04:05:48 +0900

    chore: udpate server

--- server.ts
+++ server.ts
@@ -1,8 +1,18 @@
-import express from 'express'
-const app: express.Express = express()
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hi')
-})
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+// Express
+const express = require('express')
+const app = express()
+
+// Serve static files from /public
+app.use(
+  express.static('public', {
+    setHeaders: (res, path, stat) => {
+      // Serve .wasm files with correct mime type
+      if (path.endsWith('.wasm')) {
+        res.set('Content-Type', 'application/wasm')
+      }
+    }
+  })
+)
+
+// Start server
+app.listen(2222, () => console.log('Server running on port 2222!'))
